0acd7cef13e7574f1dfcc6b3aae24d347c41fdb0
Adam Grider <adam.h.grider@gmail.com>
2019-05-12 08:44:10 -0400

    add contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Contact.css';
 
 class Contact extends Component {
 	constructor(props) {
@@ -8,8 +9,19 @@ class Contact extends Component {
 
 	render() {
 		return (
-			<div>
-				<p>Contact</p>
+			<div className="Contact">
+				<h2>Contact</h2>
+				<div className="Contact-Form">
+					<p>
+						Thanks for your interest! I'm actively seeking full-time employment beginning in mid-June 2019,
+						and I would love to help on a freelance basis with any apps or websites you want to build.
+					</p>
+					<p>
+						I'll be adding a form here to send me an email soon. Until then, please don't hesitate to reach
+						out to me on <a href="https://www.linkedin.com/in/adam-h-grider">LinkedIn</a> or{' '}
+						<a href="https://www.twitter.com/ahgrider">Twitter</a>.
+					</p>
+				</div>
 			</div>
 		);
 	}
